9ad76729a8d207d6e14ee59a730064ebb9e926a6
magisystem0408 <61937077+magisystem0408@users.noreply.github.com>
2025-04-01 17:38:31 +0100

    feat: add formula

---
+++ firestore-type-generator.rb
@@ -0,0 +1,22 @@
+class FirestoreTypeGenerator < Formula
+  desc "A tool to generate TypeScript types from Firestore schema"
+  homepage "https://github.com/magisystem0408/firestore-type-generator"
+  version "0.0.1"
+  license "MIT"
+
+  if OS.mac?
+    url "https://github.com/magisystem0408/firestore-type-generator/releases/download/v#{version}/firegen-x86_64-apple-darwin.tar.gz"
+    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  elsif OS.linux?
+    url "https://github.com/magisystem0408/firestore-type-generator/releases/download/v#{version}/firegen-x86_64-unknown-linux-gnu.tar.gz"
+    sha256 "REPLACE_WITH_ACTUAL_SHA256"
+  end
+
+  def install
+    bin.install "firegen"
+  end
+
+  test do
+    system "#{bin}/firegen", "--version"
+  end
+end 
